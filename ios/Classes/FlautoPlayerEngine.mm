95c03dc6532eece687bb8837bbfb3201e86afdff
Anuj Dev <anuj@lapsihealth.com>
2024-03-05 10:21:24 +0100

    Fix unused args in codec init

--- ios/Classes/FlautoPlayerEngine.mm
+++ ios/Classes/FlautoPlayerEngine.mm
@@ -174,8 +174,7 @@
         int  m_numChannels; // 2
         int64_t m_inBuffer; // number of feed() blocks of data in the audio buffer
 }
-
-       - (AudioEngine*)init: (FlautoPlayer*)owner
+       - (AudioEngine*)init: (FlautoPlayer*)owner codec:(t_CODEC)codec
        {
                 flutterSoundPlayer = owner;
                 engine = [[AVAudioEngine alloc] init];
